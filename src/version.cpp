ac75592669116b3fa5615a608dd5cc35017d92ce
Harris Brakmic <brakmic@gmail.com>
2018-04-12 20:46:13 +0200

    add EDGE suffix

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("DeepOnion");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
+#define CLIENT_VERSION_SUFFIX   "-EDGE"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
